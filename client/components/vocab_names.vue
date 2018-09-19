dfb738b19dc7cbe5e1ee26f6cc27c8455be68d66
chantelperucca <chantelp44@gmail.com>
2018-09-19 17:37:05 -0600

    new components

---
+++ client/components/vocab_names.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "vocab_names"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/vocab_inspector/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="page">
+        <h1> {{ title }} </h1>
+        <div class="container">
+            <h2> Vocab Names </h2>
+            <h2> {Name:} </h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data: function() {
+           return {
+               title: "Controlled Vocabularies"
+           }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .container > * {
+        display: inline-block;
+        color: #ff44e7;
+        margin-left: 200px;
+        margin-right: 100px;
+    }
+    h1 {
+        color: darkblue;
+    }
+
+</style>
