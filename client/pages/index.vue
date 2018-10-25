966d6d6520490d1a9447829930043090681632de
chantelperucca <chantelp44@gmail.com>
2018-10-25 15:23:58 -0600

    new index path

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,36 +1,95 @@
-<!--<template>-->
-    <!--<section>-->
-        <!--<h1>Home Page</h1>-->
-        <!--<p v-if="$byu.user">You're logged in</p>-->
-        <!--<p v-else>Not logged in</p>-->
+<template>
+    <div>
+        <div  v-if="getLoggedIn === false">
+            <h2> {{ getMessage }} </h2>
+        </div>
+        <div
+                v-show="getLoggedIn"
+        >
+            <v-tabs
+                    fixed-tabs
+                    class="tabPad"
+                    slider-color="black"
+            >
+                <v-tab
+                        v-for="n in getNavigationLinks"
+                        :key="n"
+                        @click="changeAPI(n)"
+                >
+                    {{ n }}
+                </v-tab>
+            </v-tabs>
 
-        <!--<nuxt-link to="/vocab_inspector"> vocab inspector </nuxt-link>-->
+            <v-layout>
+                <no-ssr>
+                    <vocab-names></vocab-names>
+                </no-ssr>
+                <div>
+                    <h2> {{ getName + ':'}} </h2>
+                    <controlled-vocabs> </controlled-vocabs>
+                </div>
+            </v-layout>
+        </div>
+    </div>
+</template>
 
-        <!--<h2>Vuetify</h2>-->
-        <!--<p>This starter also has Vuetify set up with some style modifications to bring it into compliance with the BYU style guide.</p>-->
-        <!--<div>-->
-            <!--<v-btn color="primary" @click="dialog=true">Primary</v-btn>-->
-            <!--<v-btn color="secondary">Secondary</v-btn>-->
-            <!--<v-btn color="accent">Accent</v-btn>-->
-            <!--<v-btn color="success">Success</v-btn>-->
-            <!--<v-btn color="error">Error</v-btn>-->
-            <!--<v-btn color="warning">Warning</v-btn>-->
-            <!--<v-btn color="info">Info</v-btn>-->
-        <!--</div>-->
+<script>
 
-        <!--<v-alert :value="true" type="success">Success</v-alert>-->
-        <!--<v-alert :value="true" type="info">Info</v-alert>-->
-        <!--<v-alert :value="true" type="warning">Warning</v-alert>-->
-        <!--<v-alert :value="true" type="error">Error</v-alert>-->
-    <!--</section>-->
-<!--</template>-->
+    import {mapGetters, mapActions, mapMutations} from 'vuex'
 
-<!--<script>-->
-    <!--export default {-->
+    import vocabNames from "../components/vocab_names"
+    import controlledVocabs from "../components/controlled_vocabs"
 
-    <!--}-->
-<!--</script>-->
+    export default {
+        components: {
+            vocabNames,
+            controlledVocabs
+        },
+        name: "index",
+        computed: {
+            ...mapGetters ([
+                'getName',
+                'getNavigationLinks',
+                'getLoggedIn',
+                'getMessage'
+            ])
+        },
+        methods: {
+            ...mapActions([
+                'fetchAdmVocabs',
+                'fetchComVocabs'
+            ]),
+            ...mapMutations([
+                'CHANGE_TAB',
+                "RESET_PAGE"
+            ]),
+            changeAPI: function(string, store) {
+                this.CHANGE_TAB(string)
+                this.RESET_PAGE()
+                if (string === "Admissions") {
+                    this.fetchAdmVocabs()
+                }
+                else {
+                    this.fetchComVocabs()
+                }
+            }
+        },
+        beforeMount() {
+            this.fetchAdmVocabs()
+        }
+    }
 
-<!--<style lang="stylus" scoped>-->
+</script>
 
-<!--</style>-->
+<style scoped>
+
+    h2 {
+        color: #002856;
+        padding-bottom: 15px;
+    }
+
+    .tabPad {
+        margin-bottom: 50px;
+    }
+
+</style>
